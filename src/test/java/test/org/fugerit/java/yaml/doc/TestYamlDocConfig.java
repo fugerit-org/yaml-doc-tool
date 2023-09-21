354ef830701bb3c04eda949acfa7a0074df97d23
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-09-21 19:29:05 +0200

    Increased test coverage

--- src/test/java/test/org/fugerit/java/yaml/doc/TestYamlDocConfig.java
+++ src/test/java/test/org/fugerit/java/yaml/doc/TestYamlDocConfig.java
@@ -1,10 +1,12 @@
 package test.org.fugerit.java.yaml.doc;
 
-import static org.junit.Assert.fail;
-
 import java.util.Properties;
 
+import org.fugerit.java.core.cfg.ConfigException;
+import org.fugerit.java.core.cfg.ConfigRuntimeException;
+import org.fugerit.java.core.function.SafeFunction;
 import org.fugerit.java.yaml.doc.YamlDocMain;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,25 +21,53 @@ public class TestYamlDocConfig {
 
 	private static final String ID_CATALOG = "sample";
 	
-	public void testWorkerSample( String encoding ) {
-		System.setProperty( "file.encoding", FILE_ENCODING );
-		Properties props = new Properties();
-		props.setProperty(YamlDocMain.ARG_MODE, YamlDocMain.ARG_MODE_CONFIG);
-		props.setProperty(YamlDocMain.ARG_CONFIG_PATH, CONFIG_FILE);
-		props.setProperty(YamlDocMain.ARG_ID_CATALOG, ID_CATALOG);
-		try {
+	private static final String ID_CATALOG_IT = "sample_it";
+	
+	public boolean testWorkerSample( String encoding, String idCatalog ) {
+		return SafeFunction.get( () -> {
+			boolean ok = false;
+			System.setProperty( "file.encoding", FILE_ENCODING );
+			Properties props = new Properties();
+			props.setProperty(YamlDocMain.ARG_MODE, YamlDocMain.ARG_MODE_CONFIG);
+			props.setProperty(YamlDocMain.ARG_CONFIG_PATH, CONFIG_FILE);
+			props.setProperty(YamlDocMain.ARG_ID_CATALOG, idCatalog);
 			YamlDocMain.worker( props );
 			logger.info( "Generation complete! {}" , CONFIG_FILE );
-		} catch (Exception e) {
-			String message = "Error: "+e.getMessage();
-			logger.error( message, e );
-			fail( message );
-		}
+			ok = true;
+			return ok;
+		} );
 	}
 	
 	@Test
 	public void testSample() {
-		this.testWorkerSample( FILE_ENCODING );
+		boolean ok = this.testWorkerSample( FILE_ENCODING, ID_CATALOG );
+		Assert.assertTrue( ok );
+	}
+
+	@Test
+	public void testSampleIt() {
+		boolean ok = this.testWorkerSample( FILE_ENCODING, ID_CATALOG_IT );
+		Assert.assertTrue( ok );
+	}
+	
+	@Test
+	public void testSampleFail1() {
+		Assert.assertThrows( ConfigRuntimeException.class , () -> this.testWorkerSample( FILE_ENCODING, null ) );
+	}
+	
+	@Test
+	public void testSampleFail2() {
+		Properties props = new Properties();
+		props.setProperty(YamlDocMain.ARG_MODE, YamlDocMain.ARG_MODE_SINGLE );
+		Assert.assertThrows( ConfigException.class , () -> YamlDocMain.worker( props ) );
+	}
+	
+	@Test
+	public void testSampleFail3() {
+		Properties props = new Properties();
+		props.setProperty(YamlDocMain.ARG_MODE, YamlDocMain.ARG_MODE_SINGLE );
+		props.setProperty(YamlDocMain.ARG_INPUT_YAML, "src/test/resources/sample/sample_01.yaml" );
+		Assert.assertThrows( ConfigException.class , () -> YamlDocMain.worker( props ) );
 	}
 
 }
--- src/test/java/test/org/fugerit/java/yaml/doc/TestYamlDocFacade.java
+++ src/test/java/test/org/fugerit/java/yaml/doc/TestYamlDocFacade.java
@@ -1,7 +1,5 @@
 package test.org.fugerit.java.yaml.doc;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -9,10 +7,12 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.util.Locale;
 
+import org.fugerit.java.core.function.SafeFunction;
 import org.fugerit.java.core.util.PropsIO;
 import org.fugerit.java.doc.base.config.DocConfig;
 import org.fugerit.java.yaml.doc.YamlDocConfig;
 import org.fugerit.java.yaml.doc.YamlDocFacade;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,35 +27,37 @@ public class TestYamlDocFacade {
 	
 	private static final String FILE_ENCODING = "utf-8";
 	
-	public void testWorkerSample( Locale locale, String encoding, String... outputFormats ) {
-		System.setProperty( "file.encoding", FILE_ENCODING );
-		for ( String outputFormat : outputFormats ) {
-			try ( Reader reader = new FileReader( "src/test/resources/sample/sample.yaml" );
-					OutputStream os = new FileOutputStream( new File( "target/sample_facade_"+locale+"."+outputFormat ) )) {
-				YamlDocConfig config = new YamlDocConfig( outputFormat );
-				config.setLocale( locale );
-				config.setLabelsOverride( PropsIO.loadFromClassLoader( "sample/sample-label-override.properties" ) );
-				config.setExcelTryAutoresize( true );
-				YamlDocFacade facade = new YamlDocFacade();
-				int result = facade.handle(reader, os, config);
-				logger.info( "result -> {}", result );
-			} catch (Exception e) {
-				String message = "Error: "+e.getMessage();
-				logger.error( message, e );
-				fail( message );
+	public boolean testWorkerSample( Locale locale, String encoding, String... outputFormats )  {
+		return SafeFunction.get( () -> {
+			boolean ok = false;
+			System.setProperty( "file.encoding", FILE_ENCODING );
+			for ( String outputFormat : outputFormats ) {
+				try ( Reader reader = new FileReader( "src/test/resources/sample/sample.yaml" );
+						OutputStream os = new FileOutputStream( new File( "target/sample_facade_"+locale+"."+outputFormat ) )) {
+					YamlDocConfig config = new YamlDocConfig( outputFormat );
+					config.setLocale( locale );
+					config.setLabelsOverride( PropsIO.loadFromClassLoader( "sample/sample-label-override.properties" ) );
+					config.setExcelTryAutoresize( true );
+					YamlDocFacade facade = new YamlDocFacade();
+					int result = facade.handle(reader, os, config);
+					logger.info( "result -> {}", result );
+					ok = true;
+				} 
 			}	
-		}
+			return ok;
+		} );
 	}
 	
 	@Test
 	public void testSampleEN() {
-		this.testWorkerSample( LOCALE_EN, FILE_ENCODING, DocConfig.TYPE_XML, DocConfig.TYPE_FO, DocConfig.TYPE_PDF, DocConfig.TYPE_XLSX );
+		boolean ok = this.testWorkerSample( LOCALE_EN, FILE_ENCODING, DocConfig.TYPE_XML, DocConfig.TYPE_FO, DocConfig.TYPE_PDF, DocConfig.TYPE_XLSX );
+		Assert.assertTrue( ok );
 	}
 	
 	@Test
 	public void testSampleIT() {
-		this.testWorkerSample( LOCALE_IT, FILE_ENCODING, DocConfig.TYPE_XML, DocConfig.TYPE_FO, DocConfig.TYPE_PDF, DocConfig.TYPE_XLSX );
+		boolean ok = this.testWorkerSample( LOCALE_IT, FILE_ENCODING, DocConfig.TYPE_XML, DocConfig.TYPE_FO, DocConfig.TYPE_PDF, DocConfig.TYPE_XLSX );
+		Assert.assertTrue( ok );
 	}
-	
 	
 }
